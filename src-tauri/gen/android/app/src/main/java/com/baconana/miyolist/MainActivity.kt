1db47981878bc5e69e05b1341d046ccac54b6c18
Baconana-chan <lutenot@gmail.com>
2026-04-24 19:42:36 +0300

    Add Gradle wrapper files for Android project setup
    - Created gradle-wrapper.properties to specify Gradle distribution.
    - Added gradlew shell script for Unix-based systems to execute Gradle tasks.
    - Included gradlew.bat for Windows users to run Gradle commands.
    - Established settings.gradle to include the app module and apply Tauri settings.

---
+++ src-tauri/gen/android/app/src/main/java/com/baconana/miyolist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.baconana.miyolist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
